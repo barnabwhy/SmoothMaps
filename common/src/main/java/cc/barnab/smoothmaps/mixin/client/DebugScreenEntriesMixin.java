88f51d6826f32b17a1a2b9e4a97768b1a08af166
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2025-11-09 12:58:02 +0000

    use namespaced debug screen entry id

--- common/src/main/java/cc/barnab/smoothmaps/mixin/client/DebugScreenEntriesMixin.java
+++ common/src/main/java/cc/barnab/smoothmaps/mixin/client/DebugScreenEntriesMixin.java
@@ -2,12 +2,21 @@ package cc.barnab.smoothmaps.mixin.client;
 
 import cc.barnab.smoothmaps.client.SmoothMapsDebugEntry;
 import net.minecraft.client.gui.components.debug.DebugScreenEntries;
+import net.minecraft.client.gui.components.debug.DebugScreenEntry;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(DebugScreenEntries.class)
 public abstract class DebugScreenEntriesMixin {
+
+    @Shadow
+    private static ResourceLocation register(ResourceLocation arg, DebugScreenEntry arg2) {
+        return null;
+    }
+
     @Unique
-    private static final ResourceLocation SMOOTHMAPS_STATS = DebugScreenEntries.register("smoothmaps_stats", new SmoothMapsDebugEntry());
+    private static final ResourceLocation SMOOTHMAPS_STATS =
+            register(ResourceLocation.fromNamespaceAndPath("smoothmaps", "stats"), new SmoothMapsDebugEntry());
 }
